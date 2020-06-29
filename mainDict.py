81e4a026d6b16964b3411e845e8ee58484986e2a
Sahil Hakimi <sahil.hakimi@mail.utoronto.ca>
2020-06-29 17:21:29 -0400

    initial commit

---
+++ mainDict.py
@@ -0,0 +1,32 @@
+import json
+import difflib
+
+def extractDefiniton(dict,key):
+    if dict.get(key.lower()) == None:
+        if difflib.get_close_matches(key,dict.keys()) == []:
+             return "Word doesn't exit. Double check and try again"
+        else:
+            response = input("Did you mean %s instead? Enter Y for yes or N for no: " % (difflib.get_close_matches(key,dict.keys())[0]))
+            if(response == 'Y'):
+                return dict[difflib.get_close_matches(key,dict.keys())[0]]
+            else:
+                return "Word doesn't exit. Double check and try again"
+            
+
+    return (dict[key.lower()])
+
+    
+
+
+
+
+
+
+dictionary = json.load(open("data.json"))
+word = input("Enter word: ")
+output = extractDefiniton(dictionary, word)
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
